35a13e090151980b79653d8a6d55e9982d5f0970
Nick Stenning <nick@whiteink.com>
2012-07-24 17:38:42 +0100

    Bump version -> v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'elasticbackup',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = find_packages(),
 
     # metadata for upload to PyPI
